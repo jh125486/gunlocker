f5f306526f71f8d83e168ffad13fa3969f8a8e11
Jacob Hochstetler <jacob.hochstetler@gmail.com>
2012-02-17 10:24:56 -0600

    Initial Commit

---
+++ Gun Locker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gun Locker
+//
+//  Created by Jacob Hochstetler on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gun Locker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gun Locker
+//
+//  Created by Jacob Hochstetler on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
